2611bdf6edc441d3377cd087aa33fd974ece7e06
MyselfRebel <56184411+MyselfRebel@users.noreply.github.com>
2024-10-13 02:03:55 +0530

    tryagain fixed

--- src/ui/pages/QuizPage/components/QuizResult.jsx
+++ src/ui/pages/QuizPage/components/QuizResult.jsx
@@ -1,9 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "../../../../styles/pages/quiz-page/quiz-result.scss";
 import CircularProgressBar from "../../../components/CircularProgressBar";
 
 export default function QuizResult({ dialogRef, correct, total, quizUrl }) {
     const perc = Math.round((correct/total)*100);
+
+    const navigate = useNavigate();
+
+    const tryAgain = () => {
+        navigate('/quiz', {
+            state: { from: 'QuizResult', quizUrl: quizUrl },
+        });
+        window.location.reload();
+    };
 
     return (
         <dialog className="quiz-result-dialog popup-dialog" ref={dialogRef}>
@@ -14,7 +23,7 @@ export default function QuizResult({ dialogRef, correct, total, quizUrl }) {
                 <span className="score-text">Your Score {correct} out of {total}</span>
 
                 <div className="buttons">
-                    <Link to="/quiz" state={{from: "QuizResult", quizUrl: quizUrl}} className="btn btn--primary">Try Again</Link>
+                    <button onClick={tryAgain} className="btn btn--primary">Try Again</button>
                     <Link to="/" className="btn btn--secondary">Go To Home</Link>
                 </div>
             </div>
